a554198620fbb30aaaaea02c37ef925a8d793f10
Unzala <unzala@aeiota.com>
2024-01-08 22:21:33 +0530

    Log in page

---
+++ ReadMe.md
@@ -0,0 +1,3 @@
+# Spotify Clone
+- Built using Vanilla JS , Vite and tailwind CSS
+- Integrated spotify's web API for the user authentication and user playlist acess
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,6 +2,11 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
+    colors: {
+      'green': '#1Db954',
+      'white': '#FFFFFF',
+      'dark': '#191414'
+    },
     extend: {},
   },
   plugins: [],
